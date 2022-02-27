52a7fc57072f9976fd32bc9f490849da2a5792a7
UTKARSH SHUKLA <uttkarsh.shuklas1996@gmail.com>
2022-02-27 19:58:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    CACHE_TYPE = 'redis'
+    CACHE_REDIS_HOST = 'localhost'
+    CACHE_REDIS_PORT = 6379
+    CACHE_REDIS_DB = 0
+    CACHE_REDIS_URL = 'redis://localhost:6379/0'
